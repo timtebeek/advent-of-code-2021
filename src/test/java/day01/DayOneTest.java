33c0c8d56a9eb188cdf0be2b0e217c5199e8eb89
Tim te Beek <tim.te.beek@jdriven.com>
2021-12-01 18:57:47 +0100

    Slight simplification

--- src/test/java/day01/DayOneTest.java
+++ src/test/java/day01/DayOneTest.java
@@ -25,45 +25,33 @@ class DayOneTest {
 
 	@Test
 	void partOneSample() throws Exception {
-		Stream<String> lines = Stream.of(SAMPLE);
-		assertThat(countIncreasesPerPair(lines)).isEqualTo(7);
+		assertThat(countIncreasesPerOffset(Stream.of(SAMPLE), 1)).isEqualTo(7);
 	}
 
 	@Test
 	void partOneInput() throws Exception {
 		Stream<String> lines = Files.lines(Paths.get(getClass().getResource("input").toURI()));
-		assertThat(countIncreasesPerPair(lines)).isEqualTo(1184);
+		assertThat(countIncreasesPerOffset(lines, 1)).isEqualTo(1184);
 	}
 
 	@Test
 	void partTwoSample() throws Exception {
-		Stream<String> lines = Stream.of(SAMPLE);
-		assertThat(countIncreasesPerWindow(lines)).isEqualTo(5);
+		assertThat(countIncreasesPerOffset(Stream.of(SAMPLE), 3)).isEqualTo(5);
 	}
 
 	@Test
 	void partTwoInput() throws Exception {
 		Stream<String> lines = Files.lines(Paths.get(getClass().getResource("input").toURI()));
-		assertThat(countIncreasesPerWindow(lines)).isEqualTo(1158);
+		assertThat(countIncreasesPerOffset(lines, 3)).isEqualTo(1158);
 	}
 
-	private static int countIncreasesPerPair(Stream<String> strings) {
+	private static int countIncreasesPerOffset(Stream<String> strings, int offset) {
 		int[] ints = strings
 				.mapToInt(Integer::parseInt)
 				.toArray();
 		return IntStream
-				.range(0, ints.length - 1)
-				.map(i -> ints[i] < ints[i + 1] ? 1 : 0)
-				.sum();
-	}
-
-	private static int countIncreasesPerWindow(Stream<String> strings) {
-		int[] ints = strings
-				.mapToInt(Integer::parseInt)
-				.toArray();
-		return IntStream
-				.range(0, ints.length - 3)
-				.map(i -> ints[i] + ints[i + 1] + ints[i + 2] < ints[i + 1] + ints[i + 2] + ints[i + 3] ? 1 : 0)
+				.range(0, ints.length - offset)
+				.map(i -> ints[i] < ints[i + offset] ? 1 : 0)
 				.sum();
 	}
 
