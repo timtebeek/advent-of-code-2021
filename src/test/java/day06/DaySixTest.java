1f83bce738376c70c487766b1d25be7d80d47761
Tim te Beek <tim.te.beek@jdriven.com>
2021-12-06 09:10:00 +0100

    Day six part two

--- src/test/java/day06/DaySixTest.java
+++ src/test/java/day06/DaySixTest.java
@@ -1,13 +1,16 @@
 package day06;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.List;
+import java.util.Map;
 import java.util.stream.Stream;
 
+import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.toMap;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class DaySixTest {
@@ -26,30 +29,32 @@ class DaySixTest {
 	}
 
 	@Test
-	@Disabled
 	void partTwoSample() throws Exception {
 		assertThat(countNumberOfFishAfterNumberOfDays(SAMPLE, 256)).isEqualTo(26984457539L);
 	}
 
 	@Test
-	@Disabled
 	void partTwoInput() throws Exception {
 		String input = Files.readString(Paths.get(getClass().getResource("input").toURI()));
-		assertThat(countNumberOfFishAfterNumberOfDays(input, 256)).isEqualTo(-1);
+		assertThat(countNumberOfFishAfterNumberOfDays(input, 256)).isEqualTo(1605400130036L);
 	}
 
-	private static int countNumberOfFishAfterNumberOfDays(String input, int days) {
+	private static long countNumberOfFishAfterNumberOfDays(String input, int days) {
 		return Stream.iterate(
 				Stream.of(input.split(","))
 						.map(Integer::valueOf)
-						.toList(),
-				fish -> fish.stream()
-						.flatMap(p -> p == 0 ? Stream.of(6, 8) : Stream.of(--p))
-						.toList())
+						.collect(groupingBy(identity(), counting())),
+				fish -> fish.entrySet().stream()
+						.flatMap(entry -> entry.getKey() == 0
+								? Stream.of(
+										Map.entry(6, entry.getValue()),
+										Map.entry(8, entry.getValue()))
+								: Stream.of(Map.entry(entry.getKey() - 1, entry.getValue())))
+						.collect(toMap(Map.Entry::getKey, Map.Entry::getValue, Long::sum)))
 				.limit(days + 1)
-				.mapToInt(List::size)
+				.mapToLong(map -> map.values().stream().mapToLong(Long::longValue).sum())
 				.reduce((a, b) -> b)
-				.getAsInt();
+				.getAsLong();
 	}
 
 }
