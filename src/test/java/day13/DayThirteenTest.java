8f0594ca67db43fe013868da19d7a22f9ed122bc
Tim te Beek <tim.te.beek@jdriven.com>
2021-12-13 21:42:39 +0100

    Shuffle code around

--- src/test/java/day13/DayThirteenTest.java
+++ src/test/java/day13/DayThirteenTest.java
@@ -51,8 +51,7 @@ class DayThirteenTest {
 	void partOneInput() throws Exception {
 		String input = Files.readString(Paths.get(getClass().getResource("input").toURI()));
 		Paper paper = Paper.parse(input);
-		List<Instruction> instructions = paper.instructions();
-		for (Instruction instruction : instructions) {
+		for (Instruction instruction : paper.instructions()) {
 			paper = paper.fold(instruction);
 		}
 		assertThat(paper.toString()).isEqualTo("""
@@ -70,31 +69,13 @@ record Paper(Set<Point> dots, List<Instruction> instructions) {
 
 	public static Paper parse(String sample) {
 		String[] split = sample.split("\n\n");
-		Set<Point> dots = Stream.of(split[0].split("\n"))
-				.map(line -> line.split(","))
-				.map(xy -> new Point(Integer.parseInt(xy[0]), Integer.parseInt(xy[1])))
-				.collect(toSet());
-		List<Instruction> instructions = Stream.of(split[1].split("\n"))
-				.map(line -> line.split(" ")[2])
-				.map(instr -> instr.split("="))
-				.map(xynumber -> new Instruction(xynumber[0].equals("x") ? LEFT : UP, Integer.parseInt(xynumber[1])))
-				.toList();
-		return new Paper(dots, instructions);
+		return new Paper(
+				Stream.of(split[0].split("\n")).map(Point::parse).collect(toSet()),
+				Stream.of(split[1].split("\n")).map(Instruction::parse).toList());
 	}
 
 	public Paper fold(Instruction instruction) {
-		return new Paper(dots.stream()
-				.map(p -> foldPoint(p, instruction))
-				.collect(toSet()),
-				instructions);
-	}
-
-	private static Point foldPoint(Point p, Instruction instruction) {
-		Direction direction = instruction.direction();
-		int line = instruction.line();
-		return direction == UP
-				? new Point(p.x(), p.y() < line ? p.y() : line - (p.y() - line))
-				: new Point(p.x() < line ? p.x() : line - (p.x() - line), p.y());
+		return new Paper(dots.stream().map(p -> p.foldAlong(instruction)).collect(toSet()), instructions);
 	}
 
 	@Override
@@ -102,22 +83,32 @@ record Paper(Set<Point> dots, List<Instruction> instructions) {
 		int maxx = dots.stream().mapToInt(Point::x).max().getAsInt();
 		int maxy = dots.stream().mapToInt(Point::y).max().getAsInt();
 		return IntStream.rangeClosed(0, maxy)
-				.mapToObj(y -> drawLine(maxx, y))
+				.mapToObj(y -> IntStream.rangeClosed(0, maxx)
+						.mapToObj(x1 -> dots.contains(new Point(x1, y)) ? "#" : ".")
+						.collect(joining()))
 				.collect(joining("\n"));
-	}
-
-	private String drawLine(int maxx, int y) {
-		return IntStream.rangeClosed(0, maxx)
-				.mapToObj(x -> dots.contains(new Point(x, y)) ? "#" : ".")
-				.collect(joining());
 	}
 
 }
 
 record Point(int x, int y) {
+	static Point parse(String line) {
+		String[] xy = line.split(",");
+		return new Point(Integer.parseInt(xy[0]), Integer.parseInt(xy[1]));
+	}
+
+	Point foldAlong(Instruction instruction) {
+		return new Point(
+				x < instruction.line() || instruction.direction() == UP ? x : 2 * instruction.line() - x,
+				y < instruction.line() || instruction.direction() == LEFT ? y : 2 * instruction.line() - y);
+	}
 }
 
 record Instruction(Direction direction, int line) {
+	static Instruction parse(String line) {
+		String[] dirline = line.split(" ")[2].split("=");
+		return new Instruction(dirline[0].equals("x") ? LEFT : UP, Integer.parseInt(dirline[1]));
+	}
 }
 
 enum Direction {
