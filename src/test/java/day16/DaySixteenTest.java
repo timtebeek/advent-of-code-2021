94afeb56c5fbc1b7307ce795f7af71386b3db62d
Tim te Beek <tim.te.beek@jdriven.com>
2021-12-17 23:50:05 +0100

    Day sixteen initial parser

---
+++ src/test/java/day16/DaySixteenTest.java
@@ -0,0 +1,164 @@
+package day16;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.joining;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class DaySixteenTest {
+
+	private static final String SAMPLE = """
+			""";
+
+	@Test
+	@Disabled
+	void partOneSample() {
+		assertThat(something(SAMPLE)).isEqualTo(-1);
+	}
+
+	private int something(String sample) {
+		// TODO Auto-generated method stub
+		return -1;
+	}
+
+	@Test
+	void parseLiteralTest() {
+		String bits = Parser.hexToBits("D2FE28");
+		Packet packet = Parser.parseFirstPacket(bits, 0);
+		assertThat(packet.startPosition()).isEqualTo(0);
+		assertThat(packet.endPosition()).isEqualTo(24);
+		assertThat(packet.version()).isEqualTo(6);
+		assertThat(packet.type()).isEqualTo(4);
+		assertThat(packet.value()).isEqualTo(2021);
+	}
+
+	@Test
+	void parseOperatorLengthType0Test() {
+		String bits = Parser.hexToBits("38006F45291200");
+		Packet packet = Parser.parseFirstPacket(bits, 0);
+		assertThat(packet.startPosition()).isEqualTo(0);
+		assertThat(packet.endPosition()).isEqualTo(6 + 1 + 15 + 27);
+		assertThat(packet.version()).isEqualTo(1);
+		assertThat(packet.type()).isEqualTo(6);
+		assertThat(packet.value()).isEqualTo(-1);
+	}
+
+	@Test
+	void parseOperatorLengthType1Test() {
+		String bits = Parser.hexToBits("EE00D40C823060");
+		Packet packet = Parser.parseFirstPacket(bits, 0);
+		assertThat(packet.startPosition()).isEqualTo(0);
+		assertThat(packet.endPosition()).isEqualTo(6 + 1 + 11 + 3 * 11);
+		assertThat(packet.version()).isEqualTo(7);
+		assertThat(packet.type()).isEqualTo(3);
+		assertThat(packet.value()).isEqualTo(-1);
+	}
+
+	@ParameterizedTest
+	@CsvSource(textBlock = """
+			11010001010,10
+			0101001000100100,20
+			01010000001,1
+			10010000010,2
+			00110000011,3
+			""")
+	void parseSubPackets(String packet, int literalValue) {
+		assertThat(Parser.parseFirstPacket(packet, 0).value()).isEqualTo(literalValue);
+	}
+
+}
+
+class Parser {
+
+	public static int parse(String input) {
+		String bits = hexToBits(input);
+
+		int position = 0;
+
+		Packet packet = parseFirstPacket(bits, position);
+		return packet.value();
+	}
+
+	static String hexToBits(String input) {
+		String bits = Stream.of(input.split(""))
+				.map(s -> switch (s) {
+				case "0" -> "0000";
+				case "1" -> "0001";
+				case "2" -> "0010";
+				case "3" -> "0011";
+				case "4" -> "0100";
+				case "5" -> "0101";
+				case "6" -> "0110";
+				case "7" -> "0111";
+				case "8" -> "1000";
+				case "9" -> "1001";
+				case "A" -> "1010";
+				case "B" -> "1011";
+				case "C" -> "1100";
+				case "D" -> "1101";
+				case "E" -> "1110";
+				case "F" -> "1111";
+				default -> throw new IllegalArgumentException("Unexpected value: " + s);
+				})
+				.collect(joining());
+		return bits;
+	}
+
+	static Packet parseFirstPacket(String bits, int startPosition) {
+		int position = startPosition;
+		int packetVersion = Integer.parseInt(bits.substring(position, position + 3), 2);
+		int packetType = Integer.parseInt(bits.substring(position + 3, position + 6), 2);
+		position += 6;
+
+		// Parse literal
+		if (packetType == 4) {
+			StringBuilder sb = new StringBuilder();
+			while (bits.charAt(position) == '1') {
+				String group = bits.substring(position + 1, position + 5);
+				sb.append(group);
+				position += 5;
+			}
+			// Last group
+			sb.append(bits.substring(position + 1, position + 5));
+			position = position + 5;
+
+			// Consume trailing zeroes
+			while (position % 4 != 0) {
+				position++;
+			}
+			int literalValue = Integer.parseInt(sb.toString(), 2);
+			return new Packet(startPosition, position, packetVersion, packetType, literalValue);
+		}
+
+		// Parse operator and sub packets
+		char lengthTypeId = bits.charAt(position);
+		position++;
+
+		// the next 15 bits are a number that represents the total length in bits of the sub-packets contained
+		if (lengthTypeId == '0') {
+			int totalLengthInBits = Integer.parseInt(bits.substring(position, position + 15), 2);
+			position += 15;
+			position += totalLengthInBits;
+			return new Packet(startPosition, position, packetVersion, packetType, -1);
+		}
+
+		// the next 11 bits are a number that represents the number of sub-packets immediately contained
+		int numberOfSubPacketsImmediatelyContained = Integer.parseInt(bits.substring(position, position + 11), 2);
+		position += 11;
+
+		for (int i = 0; i < numberOfSubPacketsImmediatelyContained; i++) {
+			position += 11;
+		}
+
+		return new Packet(startPosition, position, packetVersion, packetType, -1);
+	}
+
+}
+
+record Packet(int startPosition, int endPosition, int version, int type, int value) {
+}
